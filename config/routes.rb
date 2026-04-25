850d21cf77d648f9ec8296d178b58c123f817f04
Julio Budal <juliobudal@hisofi.com>
2026-04-25 10:57:23 -0300

    feat(auth): split sessions into family_session + profile_session routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,28 +1,27 @@
 Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
-  root "sessions#index"
+  root "home#index"
 
-  resources :sessions, only: [ :index, :create ] do
-    collection do
-      delete :destroy
-    end
-  end
+  resource :family_session,  only: [ :new, :create, :destroy ]
+  resource :profile_session, only: [ :new, :create, :destroy ]
 
   resource :registration, only: [ :new, :create ]
   resource :password_reset, only: [ :new, :create, :edit, :update ]
 
-  get "invitations/:token/accept" => "invitations#show", as: :invitation_acceptance
+  get  "invitations/:token/accept" => "invitations#show",   as: :invitation_acceptance
   post "invitations/:token/accept" => "invitations#accept", as: :accept_invitation
 
   namespace :parent do
     root "dashboard#index"
     resources :invitations, only: [ :new, :create ]
-    resources :profiles, only: [ :index, :new, :create, :edit, :update, :destroy ]
-    resources :global_tasks, except: [ :show ] do
+    resources :profiles, only: [ :index, :new, :create, :edit, :update, :destroy ] do
       member do
-        patch :toggle_active
+        patch :reset_pin
       end
+    end
+    resources :global_tasks, except: [ :show ] do
+      member { patch :toggle_active }
     end
     resources :rewards, only: [ :index, :new, :create, :edit, :update, :destroy ]
     resources :approvals, only: [ :index ] do
@@ -44,14 +43,10 @@ Rails.application.routes.draw do
   namespace :kid do
     root to: "dashboard#index"
     resources :missions, only: [] do
-      member do
-        patch :complete
-      end
+      member { patch :complete }
     end
     resources :rewards, only: [ :index ] do
-      member do
-        post :redeem
-      end
+      member { post :redeem }
     end
     resources :wallet, only: [ :index ]
   end
