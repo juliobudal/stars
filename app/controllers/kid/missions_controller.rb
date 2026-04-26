5e05ec1d50125d1ea2454fb8454db467cba293d6
Julio Budal <juliobudal@hisofi.com>
2026-04-26 20:52:14 -0300

    feat(kid): MissionsController#new/create plus submission_comment on complete

--- app/controllers/kid/missions_controller.rb
+++ app/controllers/kid/missions_controller.rb
@@ -2,9 +2,29 @@ class Kid::MissionsController < ApplicationController
   include Authenticatable
   before_action :require_child!
 
+  def new
+    @categories = current_profile.family.categories.order(:name)
+  end
+
+  def create
+    result = Tasks::CreateCustomService.call(profile: current_profile, params: custom_params)
+
+    if result.success?
+      redirect_to kid_root_path, notice: "Missão enviada para aprovação dos pais! 🚀"
+    else
+      @categories = current_profile.family.categories.order(:name)
+      flash.now[:alert] = result.error
+      render :new, status: :unprocessable_entity
+    end
+  end
+
   def complete
     @profile_task = ProfileTask.includes(:global_task, profile: :family).pending.where(profile: current_profile).find(params[:id])
-    result = Tasks::CompleteService.new(profile_task: @profile_task, proof_photo: mission_params[:proof_photo]).call
+    result = Tasks::CompleteService.new(
+      profile_task: @profile_task,
+      proof_photo: complete_params[:proof_photo],
+      submission_comment: complete_params[:submission_comment]
+    ).call
 
     if result.success?
       respond_to do |format|
@@ -25,7 +45,11 @@ class Kid::MissionsController < ApplicationController
 
   private
 
-  def mission_params
-    params.permit(:proof_photo)
+  def complete_params
+    params.permit(:proof_photo, :submission_comment)
+  end
+
+  def custom_params
+    params.require(:profile_task).permit(:custom_title, :custom_description, :custom_points, :custom_category_id, :submission_comment, :proof_photo)
   end
 end
